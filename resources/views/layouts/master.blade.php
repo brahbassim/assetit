f684f770a489059a1f433a1f3d24f3afc548de52
brahbassim <brahbassim@gmail.com>
2026-03-24 13:20:59 +0100

    WIP

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,8 +23,8 @@
         @auth
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                <div class="sidebar-brand-icon">
+                    <i class="fas fa-layer-group"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">@lang('app.app_name')</div>
             </a>
